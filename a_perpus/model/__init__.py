943f28edb81e8a0a61792f65fbb704fad33177df
Krisna Dwi Andrianto <79742697+krisnadwia@users.noreply.github.com>
2021-07-08 21:10:38 +0700

    Delete a_perpus directory

--- a_perpus/model/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import buku
-from . import pinjam
-from . import pinjamdetil
--- a_perpus/model/buku.py
+++
@@ -1,8 +0,0 @@
-from odoo import models, fields, api
-
-class Buku(models.Model):
-    _name="perpus.buku"
-    _rec_name = "judul"
-    
-    judul = fields.Char("Judul", required=True, size=100)
-    deskripsi = fields.Text("Deskripsi")
